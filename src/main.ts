f8edd104aae39c246f0e3e5981629dc207b099c5
m.alibek <a.moyliyev@agrobank.uz>
2025-05-07 23:48:03 +0500

    feat: add Prisma schema and configure database connection
    - Added Prisma schema for User, Device, Question, and related models.
    - Integrated PostgreSQL as the database provider.
    - Updated package.json to include necessary dependencies for Prisma and Swagger.
    - Removed unused AppController and AppService files.
    - Configured global prefix and Swagger documentation with basic authentication in main.ts.
    - Set up global validation pipes for incoming requests.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
+    const globalPrefix = 'api/avto-test';
+    const swaggerPath = `${globalPrefix}/docs`;
+
+    app.use(
+        `/${swaggerPath}`,
+        basicAuth({
+            challenge: true,
+            users: {
+                developer: configService.get('SWAGGER_PASSWORD') || 'defaultpass',
+            },
+        })
+    );
+
+    app.setGlobalPrefix(globalPrefix);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+        })
+    );
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Avto Test API')
+        .setDescription('Swagger documentation for the Avto Test module')
+        .setVersion('1.0')
+        .addTag('avto-test')
+        .build();
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup(swaggerPath, app, swaggerDocument);
+
+    const PORT = configService.get<number>('PORT') || 3001;
+
+    await app.listen(PORT, () => {
+        console.log(`🚀 Server is running on: http://localhost:${PORT}`);
+        console.log(`📘 Swagger is running on: http://localhost:${PORT}/${swaggerPath}`);
+    });
 }
+
 bootstrap();
